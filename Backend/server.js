6d5be3d206faef89cb666b818e07c5c6281b46a4
juliogayosso <j_chalo@hotmail.com>
2022-09-29 19:26:50 -0500

    server up

---
+++ Backend/server.js
@@ -0,0 +1,19 @@
+const { app } = require("./app");
+
+const startServer = async () =>{
+   try {
+
+    const PORT = 4000
+  
+    app.listen(PORT,() => {
+        console.log('server runnig');
+    })
+    
+   } catch (error) {
+     console.log(error);
+   }
+
+   
+}
+
+startServer();
